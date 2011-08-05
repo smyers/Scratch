fd43412d07a17037195c5f26d4c24daae2a86d34
Steve Myers <smyers@mdsol.com>
2011-08-05 11:47:37 -0400

    initial commit

---
+++ GitApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // dev comment
+			Console.WriteLine("GitApp Scratch.");
+            Console.ReadKey();
+        }
+
+        static void Method1()
+        {
+            Console.WriteLine("Dev 1 Method");
+        }
+
+        static void Method2()
+		{
+			Console.WriteLine("Dev 2 Method");
+		}
+    }
+}
