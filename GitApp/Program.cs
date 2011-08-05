bcc087fc7f855654ceb7c57a945cacc49ef64924
Steve Myers <smyers@mdsol.com>
2011-08-05 13:57:46 -0400

    update comm 2

--- GitApp/Program.cs
+++ GitApp/Program.cs
@@ -21,7 +21,7 @@ namespace GitApp
 
         static void Method2()
 		{
-            // dev 2 comment expanded.
+            // dev 2 comment expanded again.
 			Console.WriteLine("Dev 2 Method");
 		}
     }
